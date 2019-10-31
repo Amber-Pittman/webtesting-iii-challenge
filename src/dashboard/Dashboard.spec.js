e645de74ae0450026560dfffefb8764536d5fb59
Amber-Pittman <amber@amberpittman.com>
2019-10-30 20:24:34 -0400

    working on Dashboard test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,12 +3,23 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
+// no default export, so we're importing everything from this library
+import * as rtl from "@testing-library/react";
+// not importing to a variable, since this just overrides jest global variables
+import "@testing-library/jest-dom/extend-expect";
+
 import Dashboard from './Dashboard';
 
-describe('<Dashboard />', () => {
-  it('should match snapshot', () => {
-    const tree = renderer.create(<Dashboard />).toJSON();
-
-    expect(tree).toMatchSnapshot();
+test("<Dashboard /> snapshot", () => {
+    const wrapper = rtl.render(<Dashboard />);
+    expect(wrapper.asFragment()).toMatchSnapshot();
   });
-});
+
+
+// describe('<Dashboard />', () => {
+//   it('should match snapshot', () => {
+//     const tree = renderer.create(<Dashboard />).toJSON();
+
+//     expect(tree).toMatchSnapshot();
+//   });
+// });
